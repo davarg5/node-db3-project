dcf8c3478d5e871b4af90fefa2d06011cf1a4de8
Daniel Vargas <davarg5@yahoo.com>
2020-12-09 21:42:12 -0500

    finished mvp

--- queries.sql
+++ queries.sql
@@ -17,11 +17,26 @@ select
 from [Order] o
 join shipper s
 on o.shipVia = s.id
---not done yet
+where date(o.orderDate) < date('2012-08-09')
 
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
+select 
+    p.productname,
+    o.quantity
+from product p
+join orderdetail o
+on p.id = o.productid
+where o.orderid = 10251
 
 
 -- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+
+select 
+    o.id,
+    c.companyname,
+    substr(c.contactname,  INSTR(c.contactname, ' ')) as "lastName"
+from [order] o
+join customer c
+on o.customerid = c.id
